88bdca6d1574f007f0654f95b645622e95675a1b
Vasuphol <baigho123@gmail.com>
2021-12-07 22:10:35 +0700

    - refactor :  setup footer and navbar
    import navbar and footer from components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,63 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </div> -->
+  <v-app id="app">
+    <template v-if="$route.path.includes('login')">
+      <v-content class="ma-4">
+        <router-view></router-view>
+      </v-content>
+    </template>
+    <template v-else-if="$route.path.includes('forgetpass')">
+      <v-content class="ma-4">
+        <router-view></router-view>
+      </v-content>
+    </template>
+    <template v-else-if="$route.path.includes('recovery')">
+      <v-content class="ma-4">
+        <router-view></router-view>
+      </v-content>
+    </template>
+    <template v-else-if="$route.path.includes('newpassword')">
+      <v-content class="ma-4">
+        <router-view></router-view>
+      </v-content>
+    </template>
+    <template v-else-if="$route.path.includes('register')">
+      <v-content class="ma-4">
+        <router-view></router-view>
+      </v-content>
+    </template>
+
+    <template v-else>
+      <Navbar />
+      <v-content class="ma-4">
+        <router-view></router-view>
+      </v-content>
+      <Footer />
+    </template>
+  </v-app>
 </template>
+
+<script>
+import Navbar from "../src/components/navbar/Navbar.vue"
+import Footer from "../src/components/footer/Footer.vue"
+export default {
+  name: "Login",
+  props: {
+    source: String,
+  },
+    components: {
+        Navbar,
+        Footer,
+    },
+
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -29,5 +80,10 @@
     }
   }
 }
-
+</style>
+<style scoped>
+.rounded-card {
+  border-radius: 15px;
+  border-color: black;
+}
 </style>
