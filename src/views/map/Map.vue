64d8302b39be3b500921e4907f43e613f09a78dd
Vasuphol <baigho123@gmail.com>
2022-01-12 22:09:24 +0700

    - feat(✨):  set padding map and create new location button in card title
    just button

--- src/views/map/Map.vue
+++ src/views/map/Map.vue
@@ -1,40 +1,89 @@
 <template>
   <v-card>
     <v-card-title>
-      <h1>แผนที่ของหอพัก</h1>
+      <v-row>
+        <v-col>
+          <h2>
+            <v-icon size="30px" color="#1145b7">mdi-google-maps </v-icon> แผนที่
+          </h2>
+        </v-col>
+        <v-col>
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="#092C7A" dark v-bind="attrs" v-on="on">
+                <v-icon> mdi-map-marker-plus </v-icon>
+                เพิ่มเขตอาคารในแผนที่
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="text-h5 grey lighten-2">
+                เพิ่มเขตอาคารในแผนที่
+              </v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <v-text-field append-icon="mdi-latitude" label="ละติจูด">
+                    </v-text-field>
+                  </v-col>
+                  <v-col>
+                    <v-text-field
+                      append-icon="mdi-longitude"
+                      label="ลองติจูด"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col>
+                      <v-text-field append-icon="mdi-map-legend" label="ชื่อเขต">
+
+                      </v-text-field>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="dialog = false">
+                  I accept
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
     </v-card-title>
     <!-- <label>
       AutoComplete
       <GmapAutocomplete @place_changed="setPlace"> </GmapAutocomplete>
       <button @click="usePlace">Add</button>
-    </label> -->
-    <!-- <br /> -->
-    <v-responsive>
-      <GmapMap
-        class="map-responsive"
-        :zoom="15"
-        map-type-id="hybrid"
-        settilt:45.
-        :center="{ lat: 14.9799, lng: 102.097771 }"
-      >
-        <GmapMarker
-          v-for="(marker, index) in markers"
-          :key="index"
-          :position="marker.position"
-        />
-        <GmapMarker
-          v-if="this.place"
-          label="★"
-          :position="{
-            lat: this.place.geometry.location.lat('14.979900'),
-            lng: this.place.geometry.location.lng('102.097771'),
-          }"
-        />
-      </GmapMap>
-    </v-responsive>
+    </label>
+    <br /> -->
+    <v-card-text>
+      <div>
+        <GmapMap
+          class="map-responsive"
+          :zoom="15"
+          map-type-id="hybrid"
+          settilt:45.
+          :center="{ lat: 14.9799, lng: 102.097771 }"
+        >
+          <GmapMarker
+            v-for="(marker, index) in markers"
+            :key="index"
+            :position="marker.position"
+          />
+          <GmapMarker
+            v-if="this.place"
+            label="★"
+            :position="{
+              lat: this.place.geometry.location.lat('14.979900'),
+              lng: this.place.geometry.location.lng('102.097771'),
+            }"
+          />
+        </GmapMap>
+      </div>
+    </v-card-text>
   </v-card>
 </template>
-
+<script src="vue-google-maps.js"></script>
 <script>
 import { gmapApi } from "vue2-google-maps";
 export default {
@@ -45,6 +94,7 @@ export default {
     return {
       markers: [],
       place: null,
+      dialog: false,
     };
   },
   description: "Autocomplete Example (#164)",
@@ -60,8 +110,8 @@ export default {
       if (this.place) {
         this.markers.push({
           position: {
-            lat: this.place.geometry.location.lat('14.979900'),
-            lng: this.place.geometry.location.lng('102.097771'),
+            lat: this.place.geometry.location.lat("14.979900"),
+            lng: this.place.geometry.location.lng("102.097771"),
           },
         });
         this.place = null;
@@ -72,17 +122,6 @@ export default {
 </script>
 
 <style scoped>
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 400;
-  font-size: 100px;
-  color: #526488;
-  letter-spacing: 1px;
-  font-size: 6em;
-}
-
 .subtitle {
   font-weight: 300;
   font-size: 3em;
