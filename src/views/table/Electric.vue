cce443148db68b3bc9563d7bbec365bb07223ca9
Vasuphol <baigho123@gmail.com>
2022-04-07 16:17:06 +0700

    - style (💎): delete white space and set format

--- src/views/table/Electric.vue
+++ src/views/table/Electric.vue
@@ -495,10 +495,8 @@
           </template>
           <!-- editor data -->
           <template v-slot:[`item.actions`]="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">
-              mdi-pencil
-            </v-icon>
-            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            <v-icon class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+            <v-icon @click="deleteItem(item)"> mdi-delete </v-icon>
           </template>
         </v-data-table>
         <!-- end data-table -->
--- src/views/table/Water.vue
+++ src/views/table/Water.vue
@@ -701,10 +701,8 @@
             </v-chip>
           </template>
           <template v-slot:[`item.actions`]="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">
-              mdi-pencil
-            </v-icon>
-            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            <v-icon class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+            <v-icon @click="deleteItem(item)"> mdi-delete </v-icon>
           </template>
         </v-data-table>
         <!-- end data-table -->
@@ -1611,7 +1609,6 @@ export default {
         },
       ];
     },
-
     nameFilter(value) {
       // If this filter has no value we just skip the entire filter.
       if (!this.NamefilterValue) {
