39678ceee68aca0d44bc39aadc5f59faa7770110
Vasuphol <baigho123@gmail.com>
2022-05-26 18:06:32 +0700

    - refactor (📦): install vuex

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import Vuelidate from 'vuelidate'
-import * as VueGoogleMaps from 'vue2-google-maps'
+import Vue from "vue";
+import Vuex from "vuex";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import Vuelidate from "vuelidate";
+import * as VueGoogleMaps from "vue2-google-maps";
 
 // vue trix
 
-Vue.config.productionTip = false
-Vue.use(Vuelidate)
+Vue.config.productionTip = false;
+Vue.use(Vuelidate);
+
+Vue.use(Vuex);
 Vue.use(VueGoogleMaps, {
   installComponents: true,
   load: {
-    key: 'AIzaSyBzlLYISGjL_ovJwAehh6ydhB56fCCpPQw',
-    libraries: 'places',
-    region: 'TH',
-    language: 'th',
-  }
+    key: "AIzaSyBzlLYISGjL_ovJwAehh6ydhB56fCCpPQw",
+    libraries: "places",
+    region: "TH",
+    language: "th",
+  },
 });
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
