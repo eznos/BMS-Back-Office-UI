e3480086b9b9b827031d33c8201f6265a14c0306
Baigho Fakku <baigho123@gmail.com>
2023-05-02 19:55:49 +0700

    - fix 🐛: fix url api

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://localhost:3000";
+const apiUrl = "https://bms-back-office-api.onrender.com";
 //"https://mbs-mockoon.herokuapp.com";
 // http://localhost:3000
 // https://bms-back-office-api.onrender.com"
